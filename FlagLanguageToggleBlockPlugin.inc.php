0068fc97e63ad6d5582066293faf00056593d841
Giovani Pieri <giovani@lepidus.com.br>
2012-09-10 16:28:12 -0300

    Plugin desabilitado por padrão

--- FlagLanguageToggleBlockPlugin.inc.php
+++ FlagLanguageToggleBlockPlugin.inc.php
@@ -5,7 +5,7 @@
  *
  * Copyright (c) 2003-2012 John Willinsky
  * Contributed by from by Lepidus Tecnologia
- * 
+ *
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
  *
  * @class FlagLanguageToggleBlockPlugin
@@ -20,14 +20,6 @@
 import('lib.pkp.classes.plugins.BlockPlugin');
 
 class FlagLanguageToggleBlockPlugin extends BlockPlugin {
-	/**
-	 * Determine whether the plugin is enabled. Overrides parent so that
-	 * the plugin will be displayed during install.
-	 */
-	function getEnabled() {
-		if (!Config::getVar('general', 'installed')) return true;
-		return parent::getEnabled();
-	}
 
 	/**
 	 * Install default settings on system install.
@@ -43,25 +35,6 @@ class FlagLanguageToggleBlockPlugin extends BlockPlugin {
 	 */
 	function getContextSpecificPluginSettingsFile() {
 		return $this->getPluginPath() . '/settings.xml';
-	}
-
-	/**
-	 * Get the block context. Overrides parent so that the plugin will be
-	 * displayed during install.
-	 * @return int
-	 */
-	function getBlockContext() {
-		if (!Config::getVar('general', 'installed')) return BLOCK_CONTEXT_RIGHT_SIDEBAR;
-		return parent::getBlockContext();
-	}
-
-	/**
-	 * Determine the plugin sequence. Overrides parent so that
-	 * the plugin will be displayed during install.
-	 */
-	function getSeq() {
-		if (!Config::getVar('general', 'installed')) return 2;
-		return parent::getSeq();
 	}
 
 	/**
