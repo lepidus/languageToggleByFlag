695885162c3be567823121a4b2b0dccd34f40b21
Leandro <leandro@lepidus.com.br>
2019-08-21 13:33:09 -0400

    Documentação adicionado ao plugin

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <?php
 /**
- * @defgroup plugins_blocks_flaglanguageToggle Flag Language Toggle Block Plugin
+ * @defgroup plugins_blocks_bandeiraIdioma Flag Language Toggle Block Plugin
  */
  
 /**
- * @file plugins/blocks/flaglanguageToggle/index.php
+ * @file plugins/blocks/bandeiraIdioma/index.php
  *
  * Copyright (c) 2014-2019 Simon Fraser University
  * Copyright (c) 2003-2019 John Willinsky
+ * Copyright (c) 2019 Lepidus Tecnologia
+ *
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
  *
- * @ingroup plugins_blocks_flaglanguageToggle
+ * @ingroup plugins_blocks_bandeiraIdioma
  * @brief Wrapper for language selector block plugin.
  *
  */
