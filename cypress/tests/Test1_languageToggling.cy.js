e902cf7f0e060cfc5c2711cee6c03b794872fdeb
Jhon <jhon@lepidus.com.br>
2024-04-03 15:53:53 -0400

    Adds integration test for adding plugin to sidebar
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#743
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

---
+++ cypress/tests/Test1_languageToggling.cy.js
@@ -0,0 +1,14 @@
+describe('Language Toggle by Flag - Toggle of language', function() {
+	it('Sets plugin to display at sidebar', function() {
+        cy.login('dbarnes', null, 'publicknowledge');
+		cy.contains('a', 'Website').click();
+        
+        cy.contains('button', 'Appearance').click();
+        cy.get('#appearance-setup-button').click();
+        cy.get('span', 'Language Toggle by Flag Block').parent().within(() => {
+            cy.get('input[name="sidebar"]').check();
+        });
+        cy.get('#appearance-setup button:contains("Save")').click();
+        cy.contains('.pkpFormPage__status', 'Saved');
+    });
+});
